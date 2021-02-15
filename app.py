acbd7127fc4d8c987551698c6dd81793ea0c8e27
bui minh hien <hbui@edu.lapinamk.fi>
2021-02-15 19:14:02 +0100

    add feature to main app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import dash
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
@@ -26,11 +27,11 @@ app = dash.Dash(__name__)
 server = app.server
 
 
-backGroundColorOption = html.Div(id = "background",children=[
-    html.P("Background Color:"),
-    dcc.RadioItems(
-        id='background-setting',
-        # className="background-setting", 
+backGroundColorOption = html.Div(className = "background-setting",children=[
+    html.P("Background Color:",className = "title-options"),
+    dcc.Dropdown(
+        id = "background-color",
+        className='background-setting',
         options=[{
                 "value" : "Dark",
                 "label" : "Dark"
@@ -40,13 +41,15 @@ backGroundColorOption = html.Div(id = "background",children=[
                 }
             ],
         value="Light",
+        clearable=False
     ),
 ])
 
-countryColorOption = html.Div(id = "country-color",children=[
-    html.P("Country Color:"),
-    dcc.RadioItems(
-        id='country-setting',
+countryColorOption = html.Div(className= "country-setting",children=[
+    html.P("Country Color:",className = "title-options"),
+    dcc.Dropdown(
+        id = "country-color",
+        className='country-setting',
         # className="background-setting", 
         options=[{
                 "value" : "plotly3",
@@ -66,47 +69,68 @@ countryColorOption = html.Div(id = "country-color",children=[
                 }
             ],
         value="plotly3",
+        clearable=False
     ),
 ])
 
-countryDropDown = dcc.Dropdown(
-        id='country-dropdown',
-        options= [{"label":country,"value":country} for country in data.country],
-        value= "Finland",
-        clearable=False
-    )
+countryDropDown = html.Div(
+    className='country-dropdown',
+    children=[
+        #className = "title-options",
+        html.P("Choose your Country",className = "title-options"),
+        dcc.Dropdown(
+            id = "country-name",
+            className='country-dropdown',
+            options= [{"label":country,"value":country} for country in data.country],
+            value= "Finland",
+            clearable=False
+            )
+    ]
+)
+    
 
 app.layout = html.Div(children=[
-    html.H1(children='Cultural Difference from your country',),
-    html.Div(children="Dash: A web application framework for Python."),
+    html.Center(
+        className = "intro",
+        children=[
+            html.H1(children='Cultural Difference from your country',),
+            html.Div(id= "subtitle",
+                children="Dash: A web application framework for Python."),
+        ]
+    ),
 
-    html.Br(),
-    backGroundColorOption,
-    html.Br(),
-    countryColorOption,
-    html.Br(),
-    countryDropDown,
-    html.Br(),
 
-    dcc.Graph(id='core-map')
+    html.Div(
+        id = 'option-setting',
+        children=[
+            countryDropDown,
+            countryColorOption,
+            backGroundColorOption
+        ]
+    ),
+    # html.Br(),
+
+    dcc.Graph(id='core-map'),
+    dash_table.DataTable(id = "core-table")
+    # columns=[ 'pdi', 'idv', 'mas', 'uai', 'ltowvs', 'ivr'],)
 ])
 
 
 @app.callback(
     Output(component_id = "core-map", component_property = 'figure'),
     [
-        Input(component_id = 'country-dropdown', component_property= 'value'),
-        Input(component_id='background-setting',component_property='value'),
-        Input(component_id='country-setting',component_property='value')
+        Input(component_id = 'country-name', component_property= 'value'),
+        Input(component_id='background-color',component_property='value'),
+        Input(component_id='country-color',component_property='value')
     ]
 )
 def updateMap(country,backgroundMode, countryColor,df = data):
+    borderColor = [1]*df.shape[0]
     colorMap = 'totalDiff'
-    if country == "":
-        country = "Finland"
+
     metricsList = [ 'pdi', 'idv', 'mas', 'uai', 'ltowvs', 'ivr']
     df[colorMap] = (df[metricsList]-df[df.country == country][metricsList].values).apply(lambda row: row.abs()).sum(axis=1)
-    df = df.dropna()
+    df = df.dropna().round(2)
     fig = px.choropleth(
         data_frame=df,
         locations="country",
@@ -115,19 +139,40 @@ def updateMap(country,backgroundMode, countryColor,df = data):
         range_color=[df[colorMap].min(),df[colorMap].max()],
         hover_data=["country","idv","mas","pdi",'uai', 'ltowvs', 'ivr'],
         color_continuous_scale = countryColor,
-        # px.colors.sequential.Plasma
         labels={colorMap:'Total different'}, 
-        template = 'xgridoff'
+        # template = 'xgridoff',
+        
     )
     fig.update_layout(
         autosize=True,
         margin={"r":10,"t":10,"l":10,"b":10},
+        # paper_bgcolor= "#FFFAF0"
         )
     if backgroundMode == "Dark":
-        fig.update_layout(paper_bgcolor='#4E5D6C')
+        fig.update_layout(geo_bgcolor='#4E5D6C')
     else:
-        fig.update_layout(paper_bgcolor='#FFFAF0')
+        fig.update_layout(geo_bgcolor='#FFFAF0')
     return fig
 
+@app.callback(
+    [
+        Output(component_id = 'core-table', component_property= 'columns'),
+        Output(component_id = 'core-table', component_property= 'data')
+    ],
+    Input(component_id = 'country-name', component_property= 'value')
+)
+def create_table(country, table = data):
+    metricsList = [ 'pdi', 'idv', 'mas', 'uai', 'ltowvs', 'ivr']
+    table['Total different'] = (table[metricsList]-table[table.country == country][metricsList].values).apply(lambda row: row.abs()).sum(axis=1)
+    table = table.dropna().round(2)
+
+    table_sorted = table.sort_values(['Total different'])
+    table_sorted = table_sorted.head(6)[1:]
+    columnsList = [{"name":eachColumn, "id":eachColumn} for eachColumn in table_sorted.columns]
+    # print(table_sorted.to_dict('records'))
+    return columnsList,table_sorted.to_dict('records')
+    
+    
+    # pass
 if __name__ == '__main__':
     app.run_server(debug=True)
